c8925d9b8779cb82e4f12a4e3ad0d08efb43141d
Tran Phuc Vinh CE182381 <phucvinhct1006@gmail.com>
2025-03-13 15:38:27 +0700

    feat:update code manage authors (crud), manage warehouse(view + import)

--- src/main/java/DAO/AuthorDAO.java
+++ src/main/java/DAO/AuthorDAO.java
@@ -82,4 +82,53 @@ public class AuthorDAO extends DBContext.DBContext{
 //        int id = dao.getrow();
 //         System.out.println(id);
     }
+     public void updateAuthor(Authors author) {
+        String sql = "UPDATE Authors SET authorName=?, status=? WHERE authorID=?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, author.getAuthorName());
+            st.setString(2, author.getStatus());
+            st.setString(3, author.getAuthorID());
+            st.executeUpdate();
+            st.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAuthor(String authorID) {
+        String sql = "UPDATE Authors SET status = 'Inactive' WHERE authorID = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, authorID);
+            st.executeUpdate();
+            st.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNextAuthorID() {
+        String sql = "SELECT MAX(CAST(SUBSTRING(authorID, 2, LEN(authorID)) AS INT)) FROM Authors";
+        String nextID = "A1";  // Giá trị mặc định
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+
+            if (rs.next()) {
+                int maxID = rs.getInt(1);
+                nextID = "A" + (maxID + 1);  // Tăng ID lên 1 và gán vào nextID
+            }
+
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return nextID;
+    }
 }
